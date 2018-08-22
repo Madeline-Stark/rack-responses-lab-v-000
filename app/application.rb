6c2f882e0534cbd85d7ae0d67401708ed4243061
Madeline Stark <madeline.stark1@gmail.com>
2018-08-22 18:41:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,16 +3,6 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Time.now
-
-    # num_1 = Kernel.rand(1..20)
-    # num_2 = Kernel.rand(1..20)
-    # num_3 = Kernel.rand(1..20)
-    #
-    # resp.write "#{num_1}\n"
-    # resp.write "#{num_2}\n"
-    # resp.write "#{num_3}\n"
-
     if Time.now(hour,min,sec) < Time.new(12,00,00)
       resp.write "Good Morning!"
     else
