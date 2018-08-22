a9e9d091c77f1e9a8a9aba72f5822f34fdae83c3
Madeline Stark <madeline.stark1@gmail.com>
2018-08-22 18:56:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,6 @@ class Application
     resp = Rack::Response.new
 
     now = Time.now
-    #noon = Time.now(hour=12, minute=0, second=0, microsecond=0)
 
     if now.hour < 12
       resp.write "Good Morning!"
