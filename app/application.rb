7e779496cad5e99491aa26abf1470f985014666b
Madeline Stark <madeline.stark1@gmail.com>
2018-08-22 18:33:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,12 +3,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Time.now 
+    time = Time.now
 
     # num_1 = Kernel.rand(1..20)
     # num_2 = Kernel.rand(1..20)
     # num_3 = Kernel.rand(1..20)
-    # 
+    #
     # resp.write "#{num_1}\n"
     # resp.write "#{num_2}\n"
     # resp.write "#{num_3}\n"
